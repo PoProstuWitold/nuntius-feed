62286fd0b8f965dd6c100b2e09d210dbed2ec8cd
PoProstuwitold <witekzawada@o2.pl>
2025-05-07 17:49:49 +0200

    feat: Subscription Articles page init

---
+++ app/components/SubscriptionArticlesClientPage.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useRef, useState } from 'react'
+
+
+export function SubscriptionArticlesClientPage() {
+
+    return (
+        <>
+            <h1 className='text-4xl font-bold lg:text-left text-center mb-4'>
+                Your Articles
+            </h1>
+        </>
+    )
+}
---
+++ app/subscriptions/articles/page.tsx
@@ -0,0 +1,12 @@
+import { SubscriptionArticlesClientPage } from '@/app/components/SubscriptionArticlesClientPage'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Your Articles',
+	description: 'Articles from your subscriptions'
+}
+
+export default async function SubscriptionsArticles() {
+
+	return <SubscriptionArticlesClientPage />
+}
